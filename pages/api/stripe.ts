5ad75477faeb9648c0cb7fdc95aa17b2aafd04e2
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-02-28 14:32:33 +0800

    ok

---
+++ pages/api/stripe.ts
@@ -0,0 +1,17 @@
+import initStripe from "stripe";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const stripe = new initStripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2022-11-15",
+});
+
+const stripecheckout = async (req: NextApiRequest, res: NextApiResponse) => {
+  //make payment intent
+  const { amount } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: Math.round(amount * 100),
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+  res.status(200).json({ client_secret: paymentIntent.client_secret });
+};
