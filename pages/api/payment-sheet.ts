676581374283774c9012ca5333a33e5e8280c790
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-03-01 15:09:07 +0800

    oko

--- pages/api/payment-sheet.ts
+++ pages/api/payment-sheet.ts
@@ -13,74 +13,96 @@ const stripecheckoutsheet = async (
   //make payment intent
   const { amount, userid } = req.body;
 
-  let customerid = "";
-
+  const customer = await stripe.customers.create();
+  const customerid = customer.id;
+  const { error } = await supabase
+    .from("profiles")
+    .update({ stripeid: customerid })
+    .match({ id: userid });
+  if (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+  //get profile from db
   if (userid) {
-    //get stripeid from db supabase
     const { data, error } = await supabase
       .from("profiles")
-      .select("stripeid")
+      .select("*")
       .eq("id", userid);
     if (error) {
       console.log(error);
       res.status(500).json({ error: error.message });
     }
-    if (data) {
-      const stripeid = data[0].stripeid;
-      if (stripeid) {
-        customerid = stripeid;
-        console.log("old customerid: ", customerid);
-      } else {
-        //create customer
-        const customer = await stripe.customers.create();
-        //update customerid
-        customerid = customer.id;
-        //update db
-        console.log("new customerid: ", customerid);
-        const { error } = await supabase
-          .from("profiles")
-          .update({ stripeid: customerid })
-          .match({ id: userid });
-        if (error) {
-          console.log(error);
-          res.status(500).json({ error: error.message });
-        }
-      }
-    }
   }
 
-  if (!customerid) {
-    const customer = await stripe.customers.create();
-    customerid = customer.id;
-    const { error } = await supabase
-      .from("profiles")
-      .update({ stripeid: customerid })
-      .match({ id: userid });
-    if (error) {
-      console.log(error);
-      res.status(500).json({ error: error.message });
-    }
-  }
+  // let customerid = "";
 
-  const ephemeralKey = await stripe.ephemeralKeys.create(
-    { customer: customerid },
-    { apiVersion: "2022-11-15" }
-  );
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: Math.round(amount * 100),
-    currency: "usd",
-    customer: customerid,
-    automatic_payment_methods: {
-      enabled: true,
-    },
-    metadata: { userid: userid },
-  });
+  // if (userid) {
+  //   //get stripeid from db supabase
+  //   const { data, error } = await supabase
+  //     .from("profiles")
+  //     .select("stripeid")
+  //     .eq("id", userid);
+  //   if (error) {
+  //     console.log(error);
+  //     res.status(500).json({ error: error.message });
+  //   }
+  //   if (data) {
+  //     const stripeid = data[0].stripeid;
+  //     if (stripeid) {
+  //       customerid = stripeid;
+  //       console.log("old customerid: ", customerid);
+  //     } else {
+  //       //create customer
+  //       const customer = await stripe.customers.create();
+  //       //update customerid
+  //       customerid = customer.id;
+  //       //update db
+  //       console.log("new customerid: ", customerid);
+  //       const { error } = await supabase
+  //         .from("profiles")
+  //         .update({ stripeid: customerid })
+  //         .match({ id: userid });
+  //       if (error) {
+  //         console.log(error);
+  //         res.status(500).json({ error: error.message });
+  //       }
+  //     }
+  //   }
+  // }
 
-  res.status(200).json({
-    ephemeralKey: ephemeralKey.secret,
-    customer: customerid,
-    clientSecret: paymentIntent.client_secret,
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
+  // if (!customerid) {
+  //   const customer = await stripe.customers.create();
+  //   customerid = customer.id;
+  //   const { error } = await supabase
+  //     .from("profiles")
+  //     .update({ stripeid: customerid })
+  //     .match({ id: userid });
+  //   if (error) {
+  //     console.log(error);
+  //     res.status(500).json({ error: error.message });
+  //   }
+  // }
+
+  // const ephemeralKey = await stripe.ephemeralKeys.create(
+  //   { customer: customerid },
+  //   { apiVersion: "2022-11-15" }
+  // );
+  // const paymentIntent = await stripe.paymentIntents.create({
+  //   amount: Math.round(amount * 100),
+  //   currency: "usd",
+  //   customer: customerid,
+  //   automatic_payment_methods: {
+  //     enabled: true,
+  //   },
+  //   metadata: { userid: userid },
+  // });
+
+  // res.status(200).json({
+  //   ephemeralKey: ephemeralKey.secret,
+  //   customer: customerid,
+  //   clientSecret: paymentIntent.client_secret,
+  //   publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  // });
 };
 export default stripecheckoutsheet;
