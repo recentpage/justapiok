7516daa6a5177b08573fe5bf883ac8bda7696189
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-03-01 14:05:42 +0800

    oko

--- pages/api/payment-sheet.ts
+++ pages/api/payment-sheet.ts
@@ -1,5 +1,6 @@
 import initStripe from "stripe";
 import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "./supabase";
 
 const stripe = new initStripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2022-11-15",
@@ -11,15 +12,49 @@ const stripecheckoutsheet = async (
 ) => {
   //make payment intent
   const { amount, userid } = req.body;
-  const customer = await stripe.customers.create();
+
+  let customerid = "";
+
+  if (userid) {
+    //get stripeid from db supabase
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("stripeid")
+      .eq("userid", userid);
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
+    if (data) {
+      const stripeid = data[0].stripeid;
+      if (stripeid) {
+        customerid = stripeid;
+      }
+    } else {
+      //create customer
+      const customer = await stripe.customers.create();
+      //update customerid
+      customerid = customer.id;
+      //update db
+      const { error } = await supabase
+        .from("profiles")
+        .update({ stripeid: customerid })
+        .eq("userid", userid);
+      if (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+      }
+    }
+  }
+
   const ephemeralKey = await stripe.ephemeralKeys.create(
-    { customer: customer.id },
+    { customer: customerid },
     { apiVersion: "2022-11-15" }
   );
   const paymentIntent = await stripe.paymentIntents.create({
     amount: Math.round(amount * 100),
     currency: "usd",
-    customer: customer.id,
+    customer: customerid,
     automatic_payment_methods: {
       enabled: true,
     },
@@ -28,7 +63,7 @@ const stripecheckoutsheet = async (
 
   res.status(200).json({
     ephemeralKey: ephemeralKey.secret,
-    customer: customer.id,
+    customer: customerid,
     clientSecret: paymentIntent.client_secret,
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   });
